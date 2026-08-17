62c43c7b44b19db7c4eff348f65ab6c23884b3ea
paulveloz28 <paulveloz28@gmail.com>
2026-08-17 01:07:17 -0500

    test: Probar constructor de Alien

--- src/com/krakedev/aliens/test/TestConstructores.java
+++ src/com/krakedev/aliens/test/TestConstructores.java
@@ -1,9 +1,22 @@
 package com.krakedev.aliens.test;
 
+import com.krakedev.aliens.Alien;
+
 public class TestConstructores {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Alien alien1 = new Alien(3, "Verde");
+		Alien alien2 = new Alien(35, "Azul");
+		
+		System.out.println("---- ALIEN 1 ----");
+		alien1.imprimir();
+		
+		System.out.println();
+		
+		System.out.println("---- ALIEN 2 ----");
+		alien2.imprimir();
 
 	}
 
